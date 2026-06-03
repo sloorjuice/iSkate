56889a444b0216588c0b1e01789b2667a10785d4
sloorjuice <antant8085@gmail.com>
2026-06-03 13:49:38 -0400

    Initial Commit

---
+++ iSkate/iSkateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSkateApp.swift
+//  iSkate
+//
+//  Created by Anthony Reynolds on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct iSkateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
